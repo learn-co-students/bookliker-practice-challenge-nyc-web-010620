3830ee5538aba0eb9cb2699114384a8b2463fce9
edhernandez04 <mchh.004@gmail.com>
2020-02-24 17:45:55 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,55 +1,76 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
+
+    let booksURL = "http://localhost:3000/books"
     
-    fetch('http://localhost:3000/books')
-        .then(resp => resp.json())
-            .then(books => getBooks(books))
-
-    function getBooks(books){
-        let ul = document.getElementById("list")
-        for (let i = 0; i < books.length; i++) {
-            let li = document.createElement('li')
-            li.id = `${books[i].id}-li`
-            li.innerHTML = books[i].title
-        ul.append(li)
-
-        li.addEventListener('click', (e) => {
-            displayDiv()
+      fetch(booksURL)
+      .then(response => response.json())
+      .then(books => books.forEach(book=> listBooks(book)))
+    
+      function listBooks(book){
+        const bookList = document.getElementById("list")
+        const li = document.createElement("li")
+        li.id = `li-${book.id}`
+        li.innerHTML = book.title
+        bookList.append(li)
+        li.addEventListener('click', () => {
+          const showPanel = document.getElementById("show-panel")
+                // let h1 = document.createElement("h1")
+                // h1.innerHTML = book.title
+                // showPanel.append(h1)
+          showPanel.innerHTML = `
+            <h1>${book.title} </h1>
+            <img src=${book.img_url}/>
+            <p>${book.description}</p>
+            <button data-id=${book.id} class="like-button"> Read me</button>
+            <ul id="user-list"> Users who have read the book: </ul>
+          `
+          book.users.forEach(user => readBooksList(user))
+    
+          let likeButton = document.querySelector(".like-button")
+          likeButton.addEventListener('click', likeButtonHandler)
+    
+            function likeButtonHandler(e){
+    
+              let bookId = e.target.dataset.id
+    
+              let data = book.users
+              let newUser = {"id":1, "username":"pouros"}
+    
+              if (data.some(user => user.username === "pouros")) {
+                  alert("You already read this!!!!")
+              } else {
+    
+              data.push(newUser)
+    
+              // let data = [{"id":1, "username":"pouros"}]
+              // book.users.forEach(user => data.push(user))
+    
+              fetch(`${booksURL}/${bookId}`, {
+                method: "PATCH",
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json'
+                  },
+                body: JSON.stringify({"users": data}),
+              })
+              .then( readBooksList(newUser) )
+    
+    
+              let userList = document.getElementById("user-list")
+            }
+    
+          // {"id":1, "username":"pouros"}
+    
+    
+            }
+            function readBooksList(user){
+              let userList = document.getElementById("user-list")
+    
+              let li = document.createElement("li")
+              li.innerHTML = user.username
+              userList.append(li)
+            }
         })
-        
-    function displayDiv(){
-        let div = document.getElementById('show-panel')
-        let readBtn = document.createElement('button')
-        readBtn.innerText = "Read Book"
-        readBtn.id = books[i].id
-        const foundBook = books[i]
-        let bookHTMLarr = foundBook.users.map(user => {
-            return `<li> ${user.username} </li>`
-        })
-        div.innerHTML = `
-        <h1>${books[i].title}</h1>
-        <img src="${books[i].img_url}">
-        <p>${books[i].description}</p>
-        ${bookHTMLarr.join("")}
-        `
-        div.append(readBtn)
-
-        readBtn.addEventListener('click', ()=>{
-            fetch("http://localhost:3000/users")
-            .then(response => response.json())
-            .then(users => {
-                bookHTMLarr.push(`<li> ${users[0].username} </li>`)
-                div.innerHTML = `
-                <h1>${books[i].title}</h1>
-                <img src="${books[i].img_url}">
-                <p>${books[i].description}</p>
-                ${bookHTMLarr.join("")}
-                `
-                div.append(readBtn)
-            })  
-        })
-    }
-
-
-        }
-    }
-});
+      }
+    
+    });
